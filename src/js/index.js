75b9d010baac691417082bd1f62c538c65ebe3f1
SHIJI.js <59438923+Oluwasijibomi@users.noreply.github.com>
2021-04-10 09:27:21 +0200

    webpack and babel setup

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
-// Global app controller
+import axios from "axios";
+import 'idempotent-babel-polyfill';
+
+async function getResults (query) {
+    const key = "28064759-4fcf-41bc-a921-e7d90e986072"
+
+    try {
+        const res = await axios(`https://forkify-api.herokuapp.com/api/v2/recipes?search=${query}&key=${key}`)
+        const recipes = res.data.data.recipes;
+        console.log(recipes)
+    } catch (error) {
+        alert(error)
+    }
+    
+}
+getResults("pizza")
+ 
