3ce486f71a343634b13fb2d95ccc7d02edb56df9
SHIJI.js <59438923+Oluwasijibomi@users.noreply.github.com>
2021-04-18 15:00:30 +0200

    Recipe controller setup

--- src/js/index.js
+++ src/js/index.js
@@ -10,7 +10,9 @@ const controlSearch = async () => {
 
     // 1.) Get Query from the view
 
-    const query = searchView.getInput(); //TODO
+    //const query = searchView.getInput(); //TODO
+
+    const query = "pizza";
 
     if (query) {
         // 2.) New search object and add it to state
@@ -21,25 +23,34 @@ const controlSearch = async () => {
         searchView.clearResults()
         renderLoader(elements.searchRes)
 
+        try {
+            // 4.) Search for recipe
+            await state.search.getResults();
 
-        // 4.) Search for recipe
-        await state.search.getResults();
-
-        // 5.) render result on UI
-        clearLoader();
-        searchView.renderResults(state.search.result)
-    }
-
-}
+            // 5.) render result on UI
+            clearLoader();
+            searchView.renderResults(state.search.result)
+        } catch (err) {
+            alert("Soemthing wrong with the Search...")
+            clearLoader();
+        };
+    };
+};
 
 elements.searchForm.addEventListener("submit", e => {
     e.preventDefault();
     controlSearch()
 });
 
+// TESTING
+window.addEventListener("load", e => {
+    e.preventDefault();
+    controlSearch()
+});
+
 elements.searchResPages.addEventListener("click", e => {
     const btn = e.target.closest(".btn-inline")
-    console.log(btn);
+    console.log(btn); 
 
     if (btn) {
         const goToPage = parseInt(btn.dataset.goto, 10);
@@ -49,6 +60,39 @@ elements.searchResPages.addEventListener("click", e => {
     }
 });
 
-const r = new Recipe("5ed6604591c37cdc054bca3b");
-r.getRecipe();
-console.log(r)
+//RECIPE CONTROLLER
+
+const controlRecipe = async () => {
+
+    // GET THE ID FROM URL
+    const id = window.location.hash.replace("#", "");
+    console.log(id)
+
+    if (id) {
+        // prepare UI for changes to
+
+        // Create new recipe object
+        state.recipe = new Recipe(id);
+
+        //Testing
+        window.r = state.recipe;
+
+        try {
+            // Get recipe dataset
+        await state.recipe.getRecipe();
+        // Calculate servings and time
+        state.recipe.calcTime();
+        state.recipe.calcServings()
+;
+        // Render recipe
+        console.log(state.recipe)
+        } catch (err) {
+            alert("Error Processing Recipe")
+        }
+    }
+}
+
+// window.addEventListener("hashchange", controlRecipe);
+// window.addEventListener("load", controlRecipe);
+
+["hashchange", "load"].forEach(event => window.addEventListener(event, controlRecipe))
--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -24,14 +24,37 @@ export default class Recipe {
     }
 
     calcTime () {
-
         // Assume that we need 15min for each 3 ingrdients
         const numIng = this.ingredients.length;
         const periods = Math.ceil(numIng / 3);
         this.time = periods * 15;
-    }
+    };
 
     calcServings() {
         this.servings = 4; 
+    };
+
+    parseIngrdients() {
+        const unitsLong = ["tablespoons", "tablespoon", "ounce", "ounces"];
+        const unitsShort = ["tbsp", "tbsp", "oz", "oz", "tsp", "tsp", "cups", "pound"];
+
+
+        const newIngredients = this.ingredients.map(el => {
+            // 1.) Uniform Units of
+            let ingredient = el.toLowerCase();
+            unitsLong.forEach((unit, i) => {
+                ingredient = ingredient.replace(unit, unitShort[i]);
+            });
+
+            // 2.) Remove Parenthesis
+            ingredients = ingredient.replace(/ *\([^)]*\) */g, "")
+
+
+            // 3.) Parse ingredients into count, unit and ingredients
+
+            return ingredients;
+        });
+        this.ingredients = newIngredients;
     }
-}
+
+};
