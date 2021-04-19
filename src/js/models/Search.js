6f3cac50cae90e17bd9da259904197d68f2c3a7d
SHIJI.js <59438923+Oluwasijibomi@users.noreply.github.com>
2021-04-19 21:36:03 +0200

    Changing the Api

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -7,6 +7,10 @@ export default class Search {
 
     async getResults (query) {
         const key = "28064759-4fcf-41bc-a921-e7d90e986072";
+
+        //https://forkify-api.herokuapp.com/api/search?q=pizza
+
+       // https://forkify-api.herokuapp.com/api/v2/recipes?search=${this.query}&key=${key}
     
         try {
             const res = await axios(`https://forkify-api.herokuapp.com/api/v2/recipes?search=${this.query}&key=${key}`)
